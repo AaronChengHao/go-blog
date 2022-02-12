cee12ff8d2d253dd8de3453c300672a61d096701
aaron <616915054@qq.com>
2022-02-12 17:16:09 +0800

    重构中间件

--- routes/web.go
+++ routes/web.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"goblog/app/http/controllers"
+	"goblog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -28,4 +29,6 @@ func RegisterWebRoutes(r *mux.Router) {
 
 	r.HandleFunc("/articles/{id:[0-9]+}/delete", ac.Delete).Methods("POST").Name("articles.delete")
 
+	// 中间件：强制内容类型为 HTML
+	r.Use(middlewares.ForceHTML)
 }
