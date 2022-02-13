9fd42ca5ca51e4ed3d05a0c10293f5d7861cb188
aaron <616915054@qq.com>
2022-02-13 22:41:56 +0800

    关联文章和分类

--- pkg/model/article/article.go
+++ pkg/model/article/article.go
@@ -3,7 +3,9 @@ package article
 import (
 	"goblog/pkg/model"
 	"goblog/pkg/model/user"
+	"goblog/pkg/pagination"
 	"goblog/pkg/route"
+	"net/http"
 	"strconv"
 )
 
@@ -11,10 +13,11 @@ import (
 
 type Article struct {
 	model.BaseModel
-	Title  string `gorm:"type:varchar(255);not null;" valid:"title"`
-	Body   string `gorm:"type:longtext;not null;" valid:"body"`
-	UserID uint64 `gorm:"not null;index"`
-	User   user.User
+	Title      string `gorm:"type:varchar(255);not null;" valid:"title"`
+	Body       string `gorm:"type:longtext;not null;" valid:"body"`
+	UserID     uint64 `gorm:"not null;index"`
+	User       user.User
+	CategoryID uint64 `gorm:"not null;default:4;index"`
 }
 
 // Link 方法用来生成文章链接
@@ -24,4 +27,21 @@ func (article Article) Link() string {
 
 func (article Article) CreatedAtDate() string {
 	return article.CreatedAt.Format("2006-01-02")
+}
+
+// GetByCategoryID 获取分类相关的文章
+func GetByCategoryID(cid string, r *http.Request, perPage int) ([]Article, pagination.ViewData, error) {
+
+	// 1. 初始化分页实例
+	db := model.DB.Model(Article{}).Where("category_id = ?", cid).Order("created_at desc")
+	_pager := pagination.New(r, db, route.Name2URL("categories.show", "id", cid), perPage)
+
+	// 2. 获取视图数据
+	viewData := _pager.Paging()
+
+	// 3. 获取数据
+	var articles []Article
+	_pager.Results(&articles)
+
+	return articles, viewData, nil
 }
--- pkg/model/category/category.go
+++ pkg/model/category/category.go
@@ -4,6 +4,8 @@ import (
 	"goblog/pkg/model"
 
 	"goblog/pkg/route"
+
+	"goblog/pkg/types"
 )
 
 // Category 文章分类
@@ -16,4 +18,15 @@ type Category struct {
 // Link 方法用来生成文章链接
 func (c Category) Link() string {
 	return route.Name2URL("categories.show", "id", c.GetStringID())
+}
+
+// Get 通过 ID 获取分类
+func Get(idstr string) (Category, error) {
+	var category Category
+	id := types.StringToUint64(idstr)
+	if err := model.DB.First(&category, id).Error; err != nil {
+		return category, err
+	}
+
+	return category, nil
 }
