52f4283c926d5e81f568a55e6d5d3211ce48ce30
aaron <616915054@qq.com>
2022-02-13 17:35:34 +0800

    配置和环境变量

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"net/http"
-
+	"fmt"
 	"goblog/app/http/middlewares"
-	"goblog/pkg/logger"
-
 	"goblog/bootstrap"
 
-	_ "github.com/go-sql-driver/mysql"
+	"goblog/pkg/config"
+	"net/http"
 )
 
+func init() {
+	// 初始化配置信息
+	// config.Initialize()
+}
+
 func main() {
+	a := config.StrMap{}
+	fmt.Println(a)
+	// 初始化 SQL
 	bootstrap.SetupDB()
+
+	// 初始化路由绑定
 	router := bootstrap.SetupRoute()
 
-	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
-	logger.LogError(err)
+	http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
 }
--- pkg/model/model.go
+++ pkg/model/model.go
@@ -1,9 +1,12 @@
 package model
 
 import (
+	"fmt"
 	"goblog/pkg/logger"
 	"goblog/pkg/types"
 	"time"
+
+	"goblog/pkg/config"
 
 	"gorm.io/gorm"
 	gormlogger "gorm.io/gorm/logger"
@@ -17,15 +20,32 @@ var DB *gorm.DB
 
 // ConnectDB 初始化模型
 func ConnectDB() *gorm.DB {
+
 	var err error
 
-	config := mysql.New(mysql.Config{
-		DSN: "go-blog:6ntnearf57PfjDtR@tcp(132.232.88.120:3306)/go-blog?charset=utf8&parseTime=True&loc=Local",
+	// 初始化 MySQL 连接信息
+	gormConfig := mysql.New(mysql.Config{
+		DSN: fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=True&loc=Local",
+			config.GetString("database.mysql.username"),
+			config.GetString("database.mysql.password"),
+			config.GetString("database.mysql.host"),
+			config.GetString("database.mysql.port"),
+			config.GetString("database.mysql.database"),
+			config.GetString("database.mysql.charset")),
 	})
 
+	var level gormlogger.LogLevel
+	if config.GetBool("app.debug") {
+		// 读取不到数据也会显示
+		level = gormlogger.Warn
+	} else {
+		// 只有错误才会显示
+		level = gormlogger.Error
+	}
+
 	// 准备数据库连接池
-	DB, err = gorm.Open(config, &gorm.Config{
-		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+	DB, err = gorm.Open(gormConfig, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(level),
 	})
 
 	logger.LogError(err)
